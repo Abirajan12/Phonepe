9ff134cd2fed7cbf91cabbf2d6f17434734f0d47
Navin Chandrasekaran <abirajan12@gmail.com>
2023-06-21 19:14:18 +0530

    br1

--- main.py
+++ main.py
@@ -3,22 +3,31 @@ from mysql_data_retrieval import *
 
 def main():
     st.set_page_config(page_title="Abi's,PhonePe Pulse 2.0", page_icon=':telephone_receiver:',layout = 'wide')
+
     with st.container():
         st.title("PhonePe pulse 2.0")
         st.write("An user friendly dashboard mimicking the Phonepe Pulse")
-    type = st.sidebar.radio("All India",('Transaction', 'User'))
-    year = int(st.sidebar.selectbox('Year',get_years()))
-    quarter = int((st.sidebar.radio('Quarter',('Q1','Q2','Q3','Q4')))[-1])
-    print(year)
-    print(quarter)
-    
-    col1, col2 = st.columns(2)
 
-    with col1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
 
-    with col2:
+    tab1, tab2  = st.tabs(['Transaction', 'User'])
+    print(tab1)
+
+    with tab1:    
+        year = st.multiselect('Year',get_years())
+        year = tuple(year)
+        print(year)
+        #quarter = (st.radio('Quarter',(['Q1','Q2','Q3','Q4']),horizontal = True))[-1]
+        quarter = st.multiselect('Quarter',(['Q1','Q2','Q3','Q4']))
+        quarter = tuple([ int(i.replace("Q","")) for i in quarter])
+        print(quarter)
+        query_List = ['Drag to choose query','Details of top 10 districts based on total count of tansactions happened in a particular year in a particular Quarter']
+        query = st.selectbox('select the Query',query_List)
+        #query_df = get_Query_Result(query,year,quarter)
+        
+        #if query_df is not None:
+        #   st.dataframe(query_df)
+
+    with tab2:
         st.header("A dog")
         st.image("https://static.streamlit.io/examples/dog.jpg")
 
--- mysql_data_retrieval.py
+++ mysql_data_retrieval.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from mysql_data_insertion import create_connection
 
 def get_years():
@@ -8,5 +9,13 @@ def get_years():
     connection.close()
     return year_list
 
-
-
+def get_Query_Result(query,year,quarter):
+    connection = create_connection()
+    cursor = connection.cursor()
+    if query == 'Drag to choose query':
+        None 
+    elif query == 'Details of top 10 districts based on total count of tansactions happened in a particular year in a particular Quarter': 
+        cursor.execute("SELECT * FROM map_transaction WHERE year = %s AND quarter = %s ORDER BY total_count DESC LIMIT 10", (year[0], quarter[0]))
+        df = pd.DataFrame(cursor.fetchall())
+        connection.close()
+        return df
