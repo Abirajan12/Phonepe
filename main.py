644b807de55b88e58fd6a838135ff16d2e0a04db
Navin Chandrasekaran <abirajan12@gmail.com>
2023-06-22 21:26:47 +0530

    User_query_done

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def main():
 
     tab1, tab2  = st.tabs(['Transaction', 'User'])
     with tab1:    
-        year = st.multiselect('Year',get_years())
-        if year:
-            year = '('+', '.join(str(value) for value in year)+')'
+        trn_year = st.multiselect('Year',get_years())
+        if trn_year:
+            trn_year = '('+', '.join(str(value) for value in trn_year)+')'
 
-        quarter = st.multiselect('Quarter',(['Q1','Q2','Q3','Q4']))
-        if quarter:
-            quarter = [ int(i.replace("Q","")) for i in quarter]
-            quarter = '('+', '.join(str(value) for value in quarter)+')'
+        trn_quarter = st.multiselect('Quarter',(['Q1','Q2','Q3','Q4']))
+        if trn_quarter:
+            trn_quarter = [ int(i.replace("Q","")) for i in trn_quarter]
+            trn_quarter = '('+', '.join(str(value) for value in trn_quarter)+')'
 
-        query_List = ['Drag to choose query','Details of top 10 districts based on total count of transactions',
+        transaction_Query_List = ['Drag to choose query','Details of top 10 districts based on total count of transactions',
                       'Total count of transactions in each payment category',
                       'Total amount of transactions in each payment category',
                       'Payment category which has maximum transactions',
@@ -30,15 +30,41 @@ def main():
                       'States to concentrate to increase transactions',
                       'Top 5 pincodes which has maximum number of transactions',
                       'Pincodes to concentrate to increase transactions']
-        query = st.selectbox('select the Query',query_List)
-        if year and quarter:
-            query_df = get_Query_Result(query,year,quarter)
-            if query_df is not None:
-                st.dataframe(query_df)
+        trn_query = st.selectbox('Select the Query',transaction_Query_List)
+        if trn_year and trn_quarter:
+            trn_query_df = get_Transaction_Query_Result(trn_query,trn_year,trn_quarter)
+            if trn_query_df is not None:
+                st.dataframe(trn_query_df)
         else:
             st.write("Please choose year/quarter")
     with tab2:
-        pass      
+        year = st.multiselect('Year ',get_years())
+        if year:
+            year = '('+', '.join(str(value) for value in year)+')'
+
+        quarter = st.multiselect('Quarter ',(['Q1','Q2','Q3','Q4']))
+        if quarter:
+            quarter = [ int(i.replace("Q","")) for i in quarter]
+            quarter = '('+', '.join(str(value) for value in quarter)+')'
+
+        user_Query_List = ['Drag to choose query','Total count of users with repect to user device',
+                      'States and their percentage of phonepe users',
+                      'State with more users with Apple',
+                      'Bottom 10 States with Xiaomi users',
+                      'Users count in year/quarter',
+                      'High number of registered users among states',
+                      'Count of app opens in a particular district',
+                      'No of Registered users, app opens in a particular quarter and particular year',
+                      'Districts with less number of Registed Users',
+                      'Top 10 states which has less number customers',
+                      'Pincodes with maximum registered users']
+        query = st.selectbox('Select the Query ',user_Query_List)
+        if year and quarter:
+            query_df = get_User_Query_Result(query,year,quarter)
+            if query_df is not None:
+                st.dataframe(query_df)
+        else:
+            st.write("Please choose year/quarter")    
 
 if __name__ == "__main__":
     main()
--- mysql_data_retrieval.py
+++ mysql_data_retrieval.py
@@ -8,7 +8,7 @@ def get_years():
         year_list = [year[0] for year in cursor.fetchall()]
     return year_list
 
-def get_Query_Result(query, year, quarter):
+def get_Transaction_Query_Result(query, year, quarter):
     if query == 'Drag to choose query':
         return None
 
@@ -165,6 +165,168 @@ def get_Query_Result(query, year, quarter):
                     GROUP BY state,pincode \
                     ORDER BY sum(total_count) \
                     LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+def get_User_Query_Result(query, year, quarter):
+    if query == 'Drag to choose query':
+        return None
+
+    elif query == 'Total count of users with repect to user device':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f"SELECT user_device, sum(total_count) \
+                    FROM aggregated_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY user_device \
+                    ORDER BY sum(total_count) DESC;"
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'States and their percentage of phonepe users':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(percentage) \
+                    FROM aggregated_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state \
+                    ORDER BY sum(percentage) DESC;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'State with more users with Apple':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(total_count) \
+                    FROM aggregated_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} AND user_device = "Apple" \
+                    GROUP BY state \
+                    ORDER BY sum(total_count) DESC;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Bottom 10 States with Xiaomi users':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(total_count) \
+                    FROM aggregated_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} AND user_device = "Xiaomi" \
+                    GROUP BY state \
+                    ORDER BY sum(total_count) \
+                    LIMIT 10;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Users count in year/quarter':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT year, quarter,sum(total_count) \
+                    FROM aggregated_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY year, quarter \
+                    ORDER BY sum(total_count);'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'High number of registered users among states':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(registered_users) \
+                    FROM map_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state \
+                    ORDER BY sum(registered_users) DESC;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Count of app opens in a particular district':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state,district, sum(app_opens) \
+                    FROM map_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state,district \
+                    ORDER BY sum(app_opens) DESC;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'No of Registered users, app opens in a particular quarter and particular year':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT year, quarter,sum(registered_users),sum(app_opens) \
+                    FROM map_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY year, quarter \
+                    ORDER BY sum(app_opens) DESC;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Districts with less number of Registed Users':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state,district, sum(registered_users) \
+                    FROM map_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state,district \
+                    ORDER BY sum(registered_users) \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Top 10 states which has less number customers':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(registered_users) \
+                    FROM top_user \
+                    WHERE year IN {year} AND quarter IN {quarter} \
+                    GROUP BY state \
+                    ORDER BY sum(registered_users) \
+                    LIMIT 10;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Pincodes with maximum registered users':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, pincode,sum(registered_users) \
+                    FROM top_user \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state,pincode \
+                    ORDER BY sum(registered_users) DESC \
+                    LIMIT 10;'
             cursor.execute(query)
             column_names = [description[0] for description in cursor.description]
             rows = cursor.fetchall()
