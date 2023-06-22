9396e29542a77f095074b21ff8178d15113cbf57
Navin Chandrasekaran <abirajan12@gmail.com>
2023-06-22 13:29:12 +0530

    Transactions_query_done

--- main.py
+++ main.py
@@ -11,18 +11,32 @@ def main():
     tab1, tab2  = st.tabs(['Transaction', 'User'])
     with tab1:    
         year = st.multiselect('Year',get_years())
-        year = '('+', '.join(str(value) for value in year)+')'
+        if year:
+            year = '('+', '.join(str(value) for value in year)+')'
 
         quarter = st.multiselect('Quarter',(['Q1','Q2','Q3','Q4']))
-        quarter = [ int(i.replace("Q","")) for i in quarter]
-        quarter = '('+', '.join(str(value) for value in quarter)+')'
+        if quarter:
+            quarter = [ int(i.replace("Q","")) for i in quarter]
+            quarter = '('+', '.join(str(value) for value in quarter)+')'
 
-        query_List = ['Drag to choose query','Details of top 10 districts based on total count of transactions']
+        query_List = ['Drag to choose query','Details of top 10 districts based on total count of transactions',
+                      'Total count of transactions in each payment category',
+                      'Total amount of transactions in each payment category',
+                      'Payment category which has maximum transactions',
+                      'Payment Category which has high amount involved in transactions',
+                      'Top 5 districts which has maximum number of transactions',
+                      'District with least number of transactions',
+                      'States which has huge amount involved in transactions',
+                      'States to concentrate to increase transactions',
+                      'Top 5 pincodes which has maximum number of transactions',
+                      'Pincodes to concentrate to increase transactions']
         query = st.selectbox('select the Query',query_List)
-        query_df = get_Query_Result(query,year,quarter)
-        
-        if query_df is not None:
-           st.dataframe(query_df)
+        if year and quarter:
+            query_df = get_Query_Result(query,year,quarter)
+            if query_df is not None:
+                st.dataframe(query_df)
+        else:
+            st.write("Please choose year/quarter")
     with tab2:
         pass      
 
--- mysql_data_retrieval.py
+++ mysql_data_retrieval.py
@@ -15,7 +15,158 @@ def get_Query_Result(query, year, quarter):
     elif query == 'Details of top 10 districts based on total count of transactions':
         with create_connection() as connection:
             cursor = connection.cursor()
-            query = f"SELECT * FROM map_transaction WHERE year IN {year} AND quarter in {quarter} ORDER BY total_count DESC LIMIT 10"
+            query = f"SELECT * \
+                    FROM map_transaction \
+                    WHERE year IN {year} AND quarter in {quarter} \
+                    ORDER BY total_count DESC LIMIT 10"
             cursor.execute(query)
-            df = pd.DataFrame(cursor.fetchall())
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Total count of transactions in each payment category':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT payment_category, sum(total_count) \
+                    FROM aggregated_transaction WHERE year IN {year}  \
+                    AND quarter IN {quarter} \
+                    GROUP BY payment_category;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Total amount of transactions in each payment category':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT payment_category, max(total_amount) \
+                    FROM aggregated_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY payment_category;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Payment category which has maximum transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT payment_category, sum(total_count) \
+                    FROM aggregated_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY payment_category \
+                    ORDER BY sum(total_count) DESC \
+                    LIMIT 1;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Payment Category which has high amount involved in transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT payment_category, sum(total_amount) \
+                    FROM aggregated_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY payment_category \
+                    ORDER BY sum(total_amount) DESC \
+                    LIMIT 1;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Top 5 districts which has maximum number of transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, district,sum(total_count) \
+                    FROM map_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state, district \
+                    ORDER BY sum(total_count) DESC \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'District with least number of transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state,district,sum(total_count) \
+                    FROM map_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state,district \
+                    ORDER BY sum(total_count) \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'States which has huge amount involved in transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(total_amount) \
+                    FROM map_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state \
+                    ORDER BY sum(total_amount) DESC \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'States to concentrate to increase transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, sum(total_count) \
+                    FROM map_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state \
+                    ORDER BY sum(total_count) \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Top 5 pincodes which has maximum number of transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state, pincode,sum(total_count) \
+                    FROM top_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state, pincode \
+                    ORDER BY sum(total_count) DESC \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
+        return df
+    
+    elif query == 'Pincodes to concentrate to increase transactions':
+        with create_connection() as connection:
+            cursor = connection.cursor()
+            query = f'SELECT state,pincode,sum(total_count) \
+                    FROM top_transaction \
+                    WHERE year IN {year}  AND quarter IN {quarter} \
+                    GROUP BY state,pincode \
+                    ORDER BY sum(total_count) \
+                    LIMIT 5;'
+            cursor.execute(query)
+            column_names = [description[0] for description in cursor.description]
+            rows = cursor.fetchall()
+            df = pd.DataFrame(rows, columns=column_names)
         return df
